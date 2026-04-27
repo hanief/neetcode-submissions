03dc67e7e5f3a9d8889b0b82b2ad0d7dc1d2c4b0
Hanief Utama <hanief@gmail.com>
2026-04-27 16:28:10 +0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 32
+print(type(variable))
+variable = 3.14
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hello"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
